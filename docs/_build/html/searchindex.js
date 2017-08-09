b500c2611eff8a0947be63a9b4ed075fc89ac301
npross <npross@lbl.gov>
2017-08-09 14:58:54 -0700

    big 2nd commit

---
+++ docs/_build/html/searchindex.js
@@ -0,0 +1,1 @@
+Search.setIndex({docnames:["index"],envversion:53,filenames:["index.rst"],objects:{},objnames:{},objtypes:{},terms:{index:0,modul:0,page:0,search:0},titles:["Welcome to miri-mrs\u2019s documentation!"],titleterms:{document:0,indic:0,miri:0,mrs:0,tabl:0,welcom:0}})
